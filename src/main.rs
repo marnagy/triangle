9f3231c62d8a64c8235435306952b7c8ed57531e
Marek Nagy <62247071@cuni.cz>
2020-08-04 20:47:06 +0200

    Make Triangle struct

---
+++ src/main.rs
@@ -0,0 +1,106 @@
+// nacitat strany, obvod, typ trojuholnika podla uhlov
+use std::io;
+
+fn main() {
+    let mut sides: [i32; 3] = [0, 0, 0];
+    let mut input = String::new();
+
+    for (i, item) in sides.iter_mut().enumerate() {
+        println!("Input side number {}", i + 1);
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                *item = match input.trim().parse() {
+                    Ok(num) => num,
+                    Err(_) => panic!("Incorrect number!"),
+                };
+            }
+            Err(_) => panic!("No input? PANIC!"),
+        }
+        input.clear();
+    }
+
+    let trngl = match Triangle::from_array(sides) {
+        Ok(triangle) => triangle,
+        Err(error) => panic!(error.kind()),
+    };
+
+    println!("Obvod trojuholnika ja {0}", trngl.triangle_circumference());
+    println!("Plocha trojuhonika je {0}", trngl.triangle_area());
+
+    if trngl.is_equilateral() {
+        println!("Trojuholnik je rovnostranny.");
+    } else {
+        println!("Trojuholnik nie je rovnostranny.");
+    }
+
+    if trngl.is_isosceles() {
+        println!("Trojuholnik je rovnoramenny.");
+    } else {
+        println!("Trojuholnik nie je rovnoramenny.");
+    }
+
+    if trngl.has_right_angle() {
+        println!("Trojuholnik je pravouhly.");
+    } else {
+        println!("Trojuholnik nie je pravouhly.");
+    }
+}
+
+struct Triangle {
+    a: i32,
+    b: i32,
+    c: i32,
+}
+
+impl Triangle {
+    fn is_equilateral(&self) -> bool {
+        self.a == self.b && self.b == self.c
+    }
+
+    fn is_isosceles(&self) -> bool {
+        (self.a == self.b && self.c != self.a)
+            || (self.b == self.c && self.a != self.b)
+            || (self.c == self.a && self.b != self.c)
+    }
+
+    fn from_array(sides: [i32; 3]) -> Result<Triangle, io::Error> {
+        if Triangle::is_valid_triangle(sides) {
+            Ok(Triangle {
+                a: sides[0],
+                b: sides[1],
+                c: sides[0],
+            })
+        } else {
+            Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "Numbers don't ",
+            ))
+        }
+    }
+
+    fn is_valid_triangle(sides: [i32; 3]) -> bool {
+        sides[0] + sides[1] > sides[2]
+            && sides[1] + sides[2] > sides[0]
+            && sides[2] + sides[0] > sides[1]
+    }
+
+    fn has_right_angle(&self) -> bool {
+        let mut sides = [self.a, self.b, self.c];
+        sides.sort();
+        sides[0] * sides[0] + sides[1] * sides[1] == sides[2] * sides[2]
+    }
+
+    fn triangle_area(&self) -> f32 {
+        let s: f32 = self.sum_sides() as f32 / 2_f32;
+        let s: f32 = (s - (self.a as f32)) * (s - (self.b as f32)) * (s - (self.c as f32)) * s;
+        s.sqrt()
+    }
+
+    fn sum_sides(&self) -> i32 {
+        self.a + self.b + self.c
+    }
+
+    fn triangle_circumference(&self) -> i32 {
+        self.sum_sides()
+    }
+}
